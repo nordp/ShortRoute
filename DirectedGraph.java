9f66b38b95e1c06f55374d4610a5a603d4725213
Philip <nordp@student.chalmers.se>
2018-02-22 12:11:04 +0100

    Complete DirectedGraph.java

--- DirectedGraph.java
+++ DirectedGraph.java
@@ -3,13 +3,22 @@ import java.util.*;
 
 public class DirectedGraph<E extends Edge> {
 
+	List<E>[] nodeList;
 
 	public DirectedGraph(int noOfNodes) {
-		;
+		nodeList = new List<E>[noOfNodes];
 	}
 
 	public void addEdge(E e) {
-		;
+		if (nodeList[e.from] == null){
+			nodeList[e.from] = new LinkedList<>();
+		}
+		nodeList[e.from].add(e);
+
+		if (nodeList[e.to] == null){
+			nodeList[e.to] = new LinkedList<>();
+		}
+		nodeList[e.to].add(e);
 	}
 
 	public Iterator<E> shortestPath(int from, int to) {	
